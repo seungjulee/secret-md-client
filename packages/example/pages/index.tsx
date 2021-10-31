89a84d9dcb6741e83a869db100aac046c6c3efb5
SeungJu Lee <sjtogo@gmail.com>
2021-10-31 14:48:38 +0100

    Remove unused pages

--- packages/example/pages/index.tsx
+++ packages/example/pages/index.tsx
@@ -1,13 +1,6 @@
 import type { NextPage } from 'next'
 import CreatePost from './create-post'
 
-const LINKS: [string, string][] = [
-  ['/guess-number', 'Guess Number'],
-  ['/btc-price-bot', 'BTC Price Bot'],
-  ['/create-post', 'Create Post'],
-  ['/view-post', 'View Post'],
-]
-
 const Home: NextPage = () => {
   return <CreatePost />
 }
